e3804a3654efe5237de9bd366e81b84ed7d06931
maxbarsukov <maximbarsukov@bk.ru>
2024-12-21 22:48:05 +0300

    feat: :sparkles: add timer

---
+++ lib/radishdb/raft/utils/timer.ex
@@ -0,0 +1,23 @@
+use Croma
+
+defmodule RadishDB.Raft.Utils.Timer do
+  @moduledoc """
+  Module for sending message to current process after given time or cancelling it.
+  """
+
+  defun make(time :: non_neg_integer, msg :: any) :: reference do
+    :erlang.send_after(time, self(), {:"$gen_cast", msg})
+  end
+
+  defun cancel(ref :: reference) :: :ok do
+    case :erlang.cancel_timer(ref) do
+      false ->
+        receive do
+          {:timeout, ^ref, _} -> :ok
+        after
+          0 -> :ok
+        end
+      _remaining_time -> :ok
+    end
+  end
+end
