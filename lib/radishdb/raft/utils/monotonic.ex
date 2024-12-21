8b0864de7e3701c990451636358b7f3502a472a9
maxbarsukov <maximbarsukov@bk.ru>
2024-12-21 22:50:21 +0300

    feat: :sparkles: add wrapper for monotonic time

---
+++ lib/radishdb/raft/utils/monotonic.ex
@@ -0,0 +1,14 @@
+use Croma
+
+defmodule RadishDB.Raft.Types.Monotonic do
+  @moduledoc """
+  Representation of monotonic time via `System.monotonic_time/0` which never decreases and does not leap.
+  """
+
+  @type t :: integer
+  defun valid?(v :: term) :: boolean, do: is_integer(v)
+
+  defun milliseconds() :: t do
+    System.monotonic_time(:millisecond)
+  end
+end
