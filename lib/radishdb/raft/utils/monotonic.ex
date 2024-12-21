cb2fe87f7a6fe742388a994c3b82565c38d308a7
maxbarsukov <maximbarsukov@bk.ru>
2024-12-21 23:48:13 +0300

    fix: :bug: fix typo in module's name

--- lib/radishdb/raft/utils/monotonic.ex
+++ lib/radishdb/raft/utils/monotonic.ex
@@ -1,6 +1,6 @@
 use Croma
 
-defmodule RadishDB.Raft.Types.Monotonic do
+defmodule RadishDB.Raft.Utils.Monotonic do
   @moduledoc """
   Representation of monotonic time via `System.monotonic_time/0` which never decreases and does not leap.
   """
