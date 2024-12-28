1ff52f5df198d91f60c25f47f4af4b6b65be1a02
maxbarsukov <maximbarsukov@bk.ru>
2024-12-28 08:06:22 +0300

    linter: :nail_care: udpate dialyzer ignore list

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,5 @@
 [
   # Warning due to `NodeId.valid?/1` which always returns `true`
   {"lib/radishdb/consensus_groups/utils/nodes_per_zone.ex", :pattern_match},
+  {"lib/radishdb/consensus_groups/cluster/manager.ex", :pattern_match},
 ]
